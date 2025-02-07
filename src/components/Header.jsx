e0ba6e45fe5871db3e1dac42eafce0b924819455
Sergio Rubio <sermessinaster@gmail.com>
2025-02-07 20:06:37 -0300

    update header-v2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 const Header = ({ currentSong, isPlaying }) => {
-  if (!currentSong) {
-    return <header className="bg-gray-800 p-4 flex items-center border-b border-gray-700">No song is selected</header>;
-  }
+  // if (!currentSong) {
+  //   return <header className="bg-gray-800 p-4 flex items-center border-b border-gray-700">No song is selected</header>;
+  // }
 
   return (
-    <header className="bg-gray-800 custom-padding p-4 flex items-center border-b border-gray-700">
+    <header className="bg-gray-800 p-4 flex custom-padding items-center justify-center border-b gap-5 border-gray-700">
       <img 
         src={currentSong.imageUrl} 
         alt={currentSong.title}
